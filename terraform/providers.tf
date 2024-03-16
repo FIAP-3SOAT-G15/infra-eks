a86783f61858a25215a3f79c4f719d95de53d5a5
Wellyson Freitas <wellyfrs@gmail.com>
2024-03-16 10:03:25 +0100

    Update IaC

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -7,9 +7,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "fiap-3soat-g15-tech-challenge-state"
+    bucket = "fiap-3soat-g15-infra-eks-state"
     key    = "live/terraform.tfstate"
-    region = "sa-east-1"
+    region = "us-east-1"
   }
 }
 
@@ -17,13 +17,13 @@ provider "aws" {
   region = var.region
 }
 
-#provider "kubernetes" {
-#  host                   = data.aws_eks_cluster.default.endpoint
-#  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
-#
-#  exec {
-#    api_version = "client.authentication.k8s.io/v1beta1"
-#    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.id]
-#    command     = "aws"
-#  }
-#}
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.default.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.id]
+    command     = "aws"
+  }
+}
